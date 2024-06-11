ee3502b9d66d96ea223a62b3cd6fca96b5c2f9e7
Romans Sigdel <romanssigdel8@gmail.com>
2024-06-11 20:16:51 +0545

    added state management to the form

--- lib/view/signup_form.dart
+++ lib/view/signup_form.dart
@@ -1,8 +1,12 @@
+import 'package:ecommerce/core/status_util.dart';
 import 'package:ecommerce/custom/custom_button.dart';
 import 'package:ecommerce/custom/custom_textformfield.dart';
+import 'package:ecommerce/provider/user_provider.dart';
+import 'package:ecommerce/utils/Helper.dart';
 import 'package:ecommerce/utils/color_const.dart';
 import 'package:ecommerce/utils/string_const.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
@@ -12,206 +16,236 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
-  String? password;
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 0, top: 0),
-                    child: Text(
-                      createAccountStr,
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  CustomTextFormField(
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return nameValidationStr;
-                      }
-                      return null;
-                    },
-                    labelText: nameStr,
-                    prefixIcon: Icon(
-                      Icons.person,
-                      color: Colors.black,
-                      size: 30,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  CustomTextFormField(
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return emailValidtionStr;
-                      }
-                      return null;
-                    },
-                    labelText: emailStr,
-                    prefixIcon: Icon(
-                      Icons.mail,
-                      color: Colors.black,
-                      size: 30,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  CustomTextFormField(
-                    onChanged: (value) {
-                      password = value;
-                    },
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return passwordValidationStr;
-                      } else if (value.length < 8) {
-                        return passwordLengthValidationStr;
-                      }
-                      return null;
-                    },
-                    labelText: passwordStr,
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Colors.black,
-                      size: 30,
-                    ),
-                    suffixIcon: Icon(
-                      Icons.visibility_off,
-                      size: 30,
-                      color: Colors.black,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  CustomTextFormField(
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return confirmPasswordValidationStr;
-                      } else if (value.length < 8) {
-                        return passwordLengthValidationStr;
-                      } else if (value != password) {
-                        return passwordMatchValidationStr;
-                      }
-                      return null;
-                    },
-                    labelText: confirmPasswordStr,
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Colors.black,
-                      size: 30,
-                    ),
-                    suffixIcon: Icon(
-                      Icons.visibility_off,
-                      size: 30,
-                      color: Colors.black,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  CustomButton(
-                    backgroundColor: buttonBackgroundColor,
-                    foregroundColor: buttonForegroundColor,
-                    onPressed: () {},
-                    child: Text(
-                      signUpButtonStr,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
+    return Consumer<UserProvider>(
+      builder: (context, userProvider, child) => SafeArea(
+        child: Scaffold(
+          body: SingleChildScrollView(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Expanded(
-                          child: Divider(
-                        indent: 15,
-                        endIndent: 5,
-                      )),
-                      Text(dividerOrStr),
-                      Expanded(
-                          child: Divider(
-                        indent: 5,
-                        endIndent: 15,
-                      )),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: 70,
-                        width: 100,
-                        child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10))),
-                            onPressed: () {},
-                            child: Icon(
-                              Icons.facebook,
-                              size: 45,
-                              color: Colors.blue,
-                            )),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 0, top: 0),
+                        child: Text(
+                          createAccountStr,
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.w900),
+                        ),
                       ),
                       SizedBox(
-                        width: 10,
+                        height: 30,
+                      ),
+                      CustomTextFormField(
+                        onChanged: (value) {
+                          userProvider.setName(value);
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return nameValidationStr;
+                          }
+                          return null;
+                        },
+                        labelText: nameStr,
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: Colors.black,
+                          size: 30,
+                        ),
                       ),
                       SizedBox(
-                        height: 70,
-                        width: 100,
-                        child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10))),
-                            onPressed: () {},
-                            child: Image.asset("assets/images/google.png")),
+                        height: 30,
                       ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        alreadyHaveAccountStr,
-                        style: TextStyle(fontSize: 16),
+                      CustomTextFormField(
+                        onChanged: (value) {
+                          userProvider.setEmail(value);
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return emailValidtionStr;
+                          }
+                          return null;
+                        },
+                        labelText: emailStr,
+                        prefixIcon: Icon(
+                          Icons.mail,
+                          color: Colors.black,
+                          size: 30,
+                        ),
                       ),
                       SizedBox(
-                        width: 50,
+                        height: 30,
                       ),
-                      Text(
-                        signInButtonStr,
-                        style:
-                            TextStyle(fontSize: 16, color: Color(0xff1161FC)),
+                      CustomTextFormField(
+                        onChanged: (value) {
+                          userProvider.setPassword(value);
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return passwordValidationStr;
+                          } else if (value.length < 8) {
+                            return passwordLengthValidationStr;
+                          }
+                          return null;
+                        },
+                        labelText: passwordStr,
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Colors.black,
+                          size: 30,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.visibility_off,
+                          size: 30,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      CustomTextFormField(
+                        onChanged: (value) {
+                          userProvider.setConfirmPassword(value);
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return confirmPasswordValidationStr;
+                          } else if (value.length < 8) {
+                            return passwordLengthValidationStr;
+                          } else if (value != userProvider.password) {
+                            return passwordMatchValidationStr;
+                          }
+                          return null;
+                        },
+                        labelText: confirmPasswordStr,
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Colors.black,
+                          size: 30,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.visibility_off,
+                          size: 30,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      CustomButton(
+                        backgroundColor: buttonBackgroundColor,
+                        foregroundColor: buttonForegroundColor,
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            await userProvider.saveStudentData();
+                            if (userProvider.saveUserStatus ==
+                                    StatusUtil.success &&
+                                userProvider.isSuccess) {
+                              Helper.displaySnackbar(
+                                  context, dataSuccessfullySavedStr);
+                            } else if (userProvider.saveUserStatus ==
+                                StatusUtil.error) {
+                              Helper.displaySnackbar(
+                                  context, userProvider.errorMessage);
+                            }
+                          }
+                        },
+                        child: Text(
+                          signUpButtonStr,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                              child: Divider(
+                            indent: 15,
+                            endIndent: 5,
+                          )),
+                          Text(dividerOrStr),
+                          Expanded(
+                              child: Divider(
+                            indent: 5,
+                            endIndent: 15,
+                          )),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 70,
+                            width: 100,
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10))),
+                                onPressed: () {},
+                                child: Icon(
+                                  Icons.facebook,
+                                  size: 45,
+                                  color: Colors.blue,
+                                )),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          SizedBox(
+                            height: 70,
+                            width: 100,
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10))),
+                                onPressed: () {},
+                                child: Image.asset("assets/images/google.png")),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            alreadyHaveAccountStr,
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(
+                            width: 50,
+                          ),
+                          Text(
+                            signInButtonStr,
+                            style: TextStyle(
+                                fontSize: 16, color: Color(0xff1161FC)),
+                          )
+                        ],
                       )
                     ],
-                  )
-                ],
+                  ),
+                ),
               ),
             ),
           ),
