a31c207fac638208ac34d49947070ee66fe3ca54
Romans Sigdel <romanssigdel8@gmail.com>
2024-09-24 09:23:10 +0545

    add navigation

--- lib/view/button_navbar.dart
+++ lib/view/button_navbar.dart
@@ -5,7 +5,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CustomBottomNavigationBar extends StatefulWidget {
-  const CustomBottomNavigationBar({super.key});
+  final int initialIndex;
+  CustomBottomNavigationBar({super.key, this.initialIndex = 0});
 
   @override
   State<CustomBottomNavigationBar> createState() =>
@@ -15,6 +16,13 @@ class CustomBottomNavigationBar extends StatefulWidget {
 class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
   List<Widget> itemList = [HomePage(), UserAccount()];
   int selectedIndex = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    selectedIndex = widget.initialIndex;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
