cae50b50ca211b81e938da0a2cef67c4106dbb39
Romans Sigdel <romanssigdel8@gmail.com>
2024-11-13 14:13:42 +0545

    add sold product to admin

--- lib/view/user_account.dart
+++ lib/view/user_account.dart
@@ -90,6 +90,7 @@ class _UserAccountState extends State<UserAccount> {
     getValue();
     getUserData();
     getProductData();
+    getOrderFromCart();
   }
 
   String? userName, userEmail, userRole;
@@ -156,6 +157,16 @@ class _UserAccountState extends State<UserAccount> {
     productProvider.setWaranty("");
     productProvider.setProductDescription("");
     setState(() {});
+  }
+
+  getOrderFromCart() async {
+    Future.delayed(
+      Duration.zero,
+      () async {
+        var provider = Provider.of<ProductProvider>(context, listen: false);
+        await provider.getOrdersFromCart();
+      },
+    );
   }
 
   @override
@@ -671,7 +682,174 @@ class _UserAccountState extends State<UserAccount> {
                                   ],
                                 ),
                               ),
-
+                            if (selectedIndex == 2)
+                              Consumer<ProductProvider>(
+                                builder: (context, productProvider, child) =>
+                                    Column(
+                                  children: [
+                                    SizedBox(
+                                      child: ListView.builder(
+                                        scrollDirection: Axis.vertical,
+                                        itemCount:
+                                            productProvider.orderList.length,
+                                        itemBuilder: (context, orderIndex) {
+                                          final order = productProvider
+                                              .orderList[orderIndex];
+                                          return Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 10.0, vertical: 10),
+                                            child: Card(
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  Padding(
+                                                    padding:
+                                                        const EdgeInsets.all(
+                                                            8.0),
+                                                    child: Text(
+                                                      "Order ID: ${order.orderId}",
+                                                      style: TextStyle(
+                                                        fontSize: 18,
+                                                        fontWeight:
+                                                            FontWeight.bold,
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  Padding(
+                                                    padding:
+                                                        const EdgeInsets.all(
+                                                            8.0),
+                                                    child: Text(
+                                                      "Order Date: ${order.orderDate?.toLocal().toString().split(' ')[0] ?? ''}",
+                                                      style: TextStyle(
+                                                          fontSize: 16),
+                                                    ),
+                                                  ),
+                                                  Padding(
+                                                    padding:
+                                                        const EdgeInsets.all(
+                                                            8.0),
+                                                    child: Text(
+                                                      "Total Amount: \Rs.${double.parse(order.totalAmount!).toStringAsFixed(2) ?? '0.00'}",
+                                                      style: TextStyle(
+                                                        fontSize: 16,
+                                                        fontWeight:
+                                                            FontWeight.bold,
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  Divider(),
+                                                  ListView.builder(
+                                                    physics:
+                                                        NeverScrollableScrollPhysics(),
+                                                    shrinkWrap: true,
+                                                    itemCount: order
+                                                            .products?.length ??
+                                                        0,
+                                                    itemBuilder: (context,
+                                                        productIndex) {
+                                                      final product =
+                                                          order.products![
+                                                              productIndex];
+                                                      return Padding(
+                                                        padding:
+                                                            const EdgeInsets
+                                                                .symmetric(
+                                                                vertical: 8.0,
+                                                                horizontal:
+                                                                    8.0),
+                                                        child: Row(
+                                                          children: [
+                                                            ClipRRect(
+                                                              borderRadius:
+                                                                  BorderRadius
+                                                                      .circular(
+                                                                          8.0),
+                                                              child:
+                                                                  Image.network(
+                                                                product.image ??
+                                                                    '',
+                                                                height: 70,
+                                                                width: 70,
+                                                                fit:
+                                                                    BoxFit.fill,
+                                                                errorBuilder:
+                                                                    (context,
+                                                                        error,
+                                                                        stackTrace) {
+                                                                  return Icon(
+                                                                    Icons
+                                                                        .broken_image,
+                                                                    size: 70,
+                                                                    color: Colors
+                                                                        .grey,
+                                                                  );
+                                                                },
+                                                                loadingBuilder:
+                                                                    (context,
+                                                                        child,
+                                                                        loadingProgress) {
+                                                                  if (loadingProgress ==
+                                                                      null)
+                                                                    return child;
+                                                                  return SizedBox(
+                                                                    height: 70,
+                                                                    width: 70,
+                                                                    child:
+                                                                        Center(
+                                                                      child:
+                                                                          CircularProgressIndicator(),
+                                                                    ),
+                                                                  );
+                                                                },
+                                                              ),
+                                                            ),
+                                                            SizedBox(width: 10),
+                                                            Expanded(
+                                                              child: Column(
+                                                                crossAxisAlignment:
+                                                                    CrossAxisAlignment
+                                                                        .start,
+                                                                children: [
+                                                                  Text(
+                                                                    "Name: ${product.name ?? ''}",
+                                                                    style: TextStyle(
+                                                                        fontSize:
+                                                                            16,
+                                                                        fontWeight:
+                                                                            FontWeight.bold),
+                                                                  ),
+                                                                  Text(
+                                                                    "Price: \Rs.${double.parse(product.price!).toStringAsFixed(2) ?? '0.00'}",
+                                                                    style: TextStyle(
+                                                                        fontSize:
+                                                                            14),
+                                                                  ),
+                                                                  Text(
+                                                                    "Quantity: ${product.quantity ?? 0}",
+                                                                    style: TextStyle(
+                                                                        fontSize:
+                                                                            14),
+                                                                  ),
+                                                                ],
+                                                              ),
+                                                            ),
+                                                          ],
+                                                        ),
+                                                      );
+                                                    },
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
                             //product list
                             if (selectedIndex == 3)
                               Consumer<ProductProvider>(
